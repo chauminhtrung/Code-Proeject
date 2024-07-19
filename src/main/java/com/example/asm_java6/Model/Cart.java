6126d44bba58a9ed45eaf73b4caf33c95ccc9426
Nguyen Tan Tai <nguyentantai0118@gmail.com>
2024-07-19 20:00:21 +0700

    taiCart update2

--- src/main/java/com/example/asm_java6/Model/Cart.java
+++ src/main/java/com/example/asm_java6/Model/Cart.java
@@ -1,2 +1,23 @@
-package com.example.asm_java6.Model;public class Cart {
+package com.example.asm_java6.Model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "Carts")
+public class Cart implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @ManyToOne
+    @JoinColumn(name ="Productid")
+    Product product;
+    @ManyToOne
+    @JoinColumn(name = "Username")
+    Account account;
+    int qty = 0;
 }
