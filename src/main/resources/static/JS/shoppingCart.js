6c9d5f0da6116edb5852764ebc33b803872222cf
Nguyen Tan Tai <nguyentantai0118@gmail.com>
2024-07-20 07:38:45 +0700

    Update

--- src/main/resources/static/JS/shoppingCart.js
+++ src/main/resources/static/JS/shoppingCart.js
@@ -3,8 +3,10 @@ let storage = localStorage.getItem('cart')
 if (storage){
     cart = JSON.parse(storage)
 }
+
+
 const addCart = async (id) => {
-    alert(id)
+
 
     const response = await axios.get(`/api-product/detail-product?id=${id}`);
     const prolist = response.data.data;
@@ -13,17 +15,15 @@ const addCart = async (id) => {
         item.qty +=1;
     }else{
         try{
-
             cart.push({prolist,qty:1})
         }catch (error) {
             console.error(error);
             container.innerHTML = '<p>Có lỗi xảy ra khi gọi API.</p>';
         }
 
-        localStorage.setItem('cart',JSON.stringify(cart))
-        viewCart(cart);
     }
-
+    localStorage.setItem('cart',JSON.stringify(cart))
+    viewCart(cart);
 }
 
 
@@ -40,11 +40,15 @@ const viewCart = (shoppingCart) =>{
                 <th scope="row"><img src="../IMG/${item.prolist.image}" width="50px"></th>
                 <td>${item.prolist.name}</td>
                 <td>${item.prolist.price}</td>
-                <td>${item.qty}</td>
+                <td>
+                <button class="btn-qty" onclick="decreaseQuantity(this)">-</button>
+                <input type="number" class="qty-input" value="${item.qty}" min="1" data-id="${item.prolist.id}">
+                <button class="btn-qty" onclick="increaseQuantity(this)">+</button></td>
                 <td><button class="btn btn-danger" onclick="removeItem(${item.prolist.id})">Del</button></td>
             </tr>
             </tbody>`
     })
+
 
 }
 
@@ -58,5 +62,37 @@ const removeItem = id =>{
     viewCart(cart)
 }
 
+
+function decreaseQuantity(button) {
+    const inputField = button.nextElementSibling;
+    let qty = parseInt(inputField.value);
+    if (qty > 1) {
+        inputField.value = qty - 1;
+        const id = inputField.dataset.id;
+        updateCartItem(id, qty - 1);
+    }
+}
+
+function increaseQuantity(button) {
+    const inputField = button.previousElementSibling;
+    let qty = parseInt(inputField.value);
+    inputField.value = qty + 1;
+    const id = inputField.dataset.id;
+    updateCartItem(id, qty + 1);
+}
+function updateCartItem(id, qty) {
+    let storage = localStorage.getItem('cart');
+    if (storage) {
+        cart = JSON.parse(storage);
+        let item = cart.find(c => c.prolist.id === id);
+        if (item) {
+            item.qty = qty;
+            localStorage.setItem('cart', JSON.stringify(cart));
+            viewCart(cart);
+        }
+
+    alert(cart)
+    }
+}
 
 viewCart(cart);
